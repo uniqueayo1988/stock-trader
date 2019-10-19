9509e4b502f19b4103aeac098ec55c7004f3e9b1
Ayo-Oluwa Adebayo <ayo-oluwa.adebayo@andela.com>
2019-10-19 22:29:13 +0100

    Add App Header

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,26 @@
 <template>
   <div id="app" class="container">
-    <router-view></router-view>
+    <app-header />
+    <div class="row">
+      <div class="col-md-12">
+        <router-view></router-view>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Header from '@/components/Header'
 export default {
   name: 'app',
   components: {
+    appHeader: Header
   }
 }
 </script>
 
 <style>
-
+  body {
+    padding: 30px;
+  }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,50 @@
 <template>
-  <h1>The Header component</h1>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <router-link to='/' class="navbar-brand" href="#">Stock Trader</router-link>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <router-link to="/portfolio" class="nav-item active" activeClass="active" tag="li">
+          <a class="nav-link">Portfolio <span class="sr-only">(current)</span></a>
+        </router-link>
+        <router-link to="/stocks" class="nav-item" activeClass="active" tag="li">
+          <a class="nav-link">Stocks</a>
+        </router-link>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Save & Load
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Save Data</a>
+            <a class="dropdown-item" href="#">Load Data</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Something else here</a>
+          </div>
+        </li>
+      </ul>
+<!--       <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form> -->
+      <div class="form-inline my-2 my-lg-0">
+        <router-link to="/portfolio" class="nav-item mr-sm-2" activeClass="active">
+          <a class="nav-link">Portfolio <span class="sr-only">(current)</span></a>
+        </router-link>
+        <div class="nav-item dropdown my-2 my-sm-0">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Save & Load
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Save Data</a>
+            <a class="dropdown-item" href="#">Load Data</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Something else here</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
 </template>
