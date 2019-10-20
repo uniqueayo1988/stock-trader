60598815109f6e63b6bd397c6a20aabaeaf3de86
Ayo-Oluwa Adebayo <ayo-oluwa.adebayo@andela.com>
2019-10-20 10:57:12 +0100

    Setup Vuex

---
+++ src/store/store.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import stocks from './modules/stocks'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  // state: {
+
+  // },
+  // mutations: {
+
+  // },
+  // actions: {
+
+  // },
+  // getters: {
+
+  // }
+  modules: {
+    stocks
+  }
+})
