f862a6d51da1cedb519f1303db03f592db202df8
Ayo-Oluwa Adebayo <ayo-oluwa.adebayo@andela.com>
2019-10-20 09:09:07 +0100

    Add stock components

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,3 +1,20 @@
 <template>
-  <h1>The Stocks -Stock component</h1>
+  <div class="col-sm-6 col-md-4 mt-4">
+    <div class="card">
+      <div class="card-header bg-muted">
+        <h4>
+          NAME
+          <small>(Price: PRICE)</small>
+        </h4>
+      </div>
+      <div class="card-body">
+        <div class="float-left">
+          <input type="number" class="form-control" placeholder="Quantity">
+        </div>
+        <div class="float-right">
+          <button class="btn btn-success">Buy</button>
+        </div>
+      </div>
+    </div>
+  </div>    
 </template>
