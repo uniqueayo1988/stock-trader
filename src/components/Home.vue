527de6a517605507035e004ce2594fa02097cab3
Ayo-Oluwa Adebayo <ayo-oluwa.adebayo@andela.com>
2019-11-02 20:05:45 +0100

    Display funds

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,18 @@
 <template>
-  <h1>The Home component</h1>
+  <div>
+    <h1>Trade or View your Portfolio</h1>
+    <h6>You may save and load your data</h6>
+    <h6>Click on 'End Day' to begin a New Day!</h6>
+    <p>Your Funds: {{ funds }}</p>
+  </div>
 </template>
+
+<script>
+  export default {
+    computed: {
+      funds () {
+        return this.$store.getters.funds
+      }
+    }
+  }
+</script>
